c5d876a6bf9f0aa6cbf9ebb9bf36928f157c0065
Dickson <dickson_hee@yahoo.com.au>
2021-12-09 23:39:49 +0800

    Updated XR Rig

--- Assets/Scripts/Core/MovementTypeManager.cs
+++ Assets/Scripts/Core/MovementTypeManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 using UnityEngine.XR.Interaction.Toolkit;
 
 namespace HorrorVR.Core
@@ -12,18 +12,27 @@ namespace HorrorVR.Core
     {
         public static MovementTypeManager current;
         
+        [Header("XR Rig")]
         [SerializeField]private CharacterController characterController;
         [SerializeField]private XRRig rig;
         
+        [Header("Movement Providers")]
         [SerializeField] private ActionBasedContinuousMoveProvider continuousMoveProvider;
         [SerializeField] private ActionBasedSnapTurnProvider snapTurnProvider;
         [SerializeField] private ActionBasedContinuousTurnProvider continuousTurnProvider;
         [SerializeField] private TeleportationProvider teleportationProvider;
         [SerializeField] private TeleportationManager teleportationManager;
+        
+        [Header("Level Transition")]
+        public Material blackoutMaterial;
+
+        private bool _isFading;
 
         private void Awake()
         {
             current = this;
+
+            StartCoroutine(LoadIntoLevel_Coroutine());
         }
 
         private void Update()
@@ -31,20 +40,21 @@ namespace HorrorVR.Core
             CapsuleFollowHeadset();
         }
 
-        public void MovementCheck()
-        {
-            if(PlayerSettings.continuousMovementEnabled) EnableContinuousMovement();
-            if(PlayerSettings.teleportMovementEnabled) EnableTeleportationMovement();
-            if(PlayerSettings.continuousTurnEnabled) EnableContinuousTurn();
-            if(PlayerSettings.snapTurnEnabled) EnableSnapTurn();
-        }
-        
         private void CapsuleFollowHeadset()
         {
             Vector3 capsuleCenter = transform.InverseTransformPoint(rig.cameraGameObject.transform.position);
 
             characterController.center = new Vector3(capsuleCenter.x, characterController.height / 2, capsuleCenter.z);
             characterController.height = rig.cameraInRigSpaceHeight + 0.2f;
+        }
+        
+        #region Movement Type Togglers
+        public void MovementCheck()
+        {
+            if(PlayerSettings.continuousMovementEnabled) EnableContinuousMovement();
+            if(PlayerSettings.teleportMovementEnabled) EnableTeleportationMovement();
+            if(PlayerSettings.continuousTurnEnabled) EnableContinuousTurn();
+            if(PlayerSettings.snapTurnEnabled) EnableSnapTurn();
         }
         
         public void EnableContinuousMovement()
@@ -54,7 +64,7 @@ namespace HorrorVR.Core
             teleportationProvider.enabled = false;
             teleportationManager.enabled = false;
         }
-        
+
         public void EnableTeleportationMovement()
         {
             continuousMoveProvider.enabled = false;
@@ -74,5 +84,50 @@ namespace HorrorVR.Core
             continuousTurnProvider.enabled = false;
             snapTurnProvider.enabled = true;
         }
+        #endregion
+        
+        #region Level Loaders
+        public void Loadlevel(string levelToLoad)
+        {
+            if (_isFading) return;
+
+            if (SceneManager.GetActiveScene().name != levelToLoad)
+            {
+                StartCoroutine(LoadLevel_Coroutine(levelToLoad));
+            }
+        }
+        
+        private IEnumerator LoadLevel_Coroutine(string levelToLoad)
+        {
+            _isFading = true;
+            
+            float startTime = Time.time;
+            Color currentColor = blackoutMaterial.color;
+            while (Time.time < startTime + 5f)
+            {
+                currentColor.a = Mathf.Lerp(0, 1, (Time.time - startTime) / 5f);
+                blackoutMaterial.color = currentColor;
+                yield return null;
+            }
+            SceneManager.LoadScene(levelToLoad);
+            _isFading = false;
+        }
+
+        private IEnumerator LoadIntoLevel_Coroutine()
+        {
+            _isFading = true;
+            
+            float startTime = Time.time;
+            Color currentColor = blackoutMaterial.color;
+            while (Time.time < startTime + 1f)
+            {
+                currentColor.a = Mathf.Lerp(1, 0, (Time.time - startTime) / 1f);
+                blackoutMaterial.color = currentColor;
+                yield return null;
+            }
+
+            _isFading = false;
+        }
+        #endregion
     }
 }
